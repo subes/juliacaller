1dc6af40851761acf0dc7aa4befe38120599abf7
Edwin Stang <gsubes@gmail.com>
2021-12-22 18:53:43 +0100

    invesdwin-oss distribution management and json dependency upgrade

--- src/test/java/org/expr/juliacaller/TestFunctionDefinition.java
+++ src/test/java/org/expr/juliacaller/TestFunctionDefinition.java
@@ -1,89 +1,86 @@
 package org.expr.juliacaller;
 
-import java.io.IOException;
-import java.util.List;
-import static org.expr.juliacaller.TestBasics.caller;
-import org.junit.jupiter.api.AfterAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class TestFunctionDefinition {
 
-    static JuliaCaller caller;
+	static JuliaCaller caller;
 
-    @BeforeAll
-    public static void init() throws IOException {
-        System.out.println("* Initializing tests");
-        // caller = new JuliaCaller("/usr/local/bin/julia", 8000);
-        // caller = new JuliaCaller("/usr/bin/julia", 8000);
-        caller = new JuliaCaller(Utilities.TryFindingJuliaExecutable(), 8001);
-        caller.startServer();
-        caller.Connect();
-    }
+	@BeforeAll
+	public static void init() throws IOException {
+		System.out.println("* Initializing tests");
+		// caller = new JuliaCaller("/usr/local/bin/julia", 8000);
+		// caller = new JuliaCaller("/usr/bin/julia", 8000);
+		caller = new JuliaCaller(Utilities.TryFindingJuliaExecutable(), 8001);
+		caller.startServer();
+		caller.Connect();
+	}
 
-    @AfterAll
-    public static void finish() throws IOException {
-        Thread th = new Thread(new Runnable() {
-            public void run() {
-                System.out.println("* Shuting down the server in 1 second(s)");
-                for (int i = 0; i < 1; i++) {
-                    try {
-                        Thread.sleep(1000);
-                        System.out.print(".");
-                    } catch (InterruptedException ie) {
+	@AfterAll
+	public static void finish() throws IOException {
+		final Thread th = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("* Shuting down the server in 1 second(s)");
+				for (int i = 0; i < 1; i++) {
+					try {
+						Thread.sleep(1000);
+						System.out.print(".");
+					} catch (final InterruptedException ie) {
 
-                    }
-                }
-            };
-        });
-        th.start();
-        try {
-            th.join();
-        } catch (InterruptedException e) {
+					}
+				}
+			};
+		});
+		th.start();
+		try {
+			th.join();
+		} catch (final InterruptedException e) {
 
-        }
-        caller.ShutdownServer();
-    }
+		}
+		caller.ShutdownServer();
+	}
 
-    @Test
-    public void defineFunctionAndCallTest() throws IOException {
-        String code = """
-                function mysum(x, y)
-                  total = x + y
-                  return total
-                end
-                """;
-        caller.ExecuteDefineFunction(code);
-        caller.Execute("jresult = mysum(3, 5)");
+	@Test
+	public void defineFunctionAndCallTest() throws IOException {
+		final String code = "function mysum(x, y)\n" //
+				+ "  total = x + y\n" //
+				+ "  return total\n" //
+				+ "end";
+		caller.ExecuteDefineFunction(code);
+		caller.Execute("jresult = mysum(3, 5)");
 
-        int result = caller.getInt("jresult");
-        assertEquals(8, result);
-    }
+		final int result = caller.getInt("jresult");
+		assertEquals(8, result);
+	}
 
-    @Test
-    public void defineFunctionAndCallTestMultipleDispatch() throws IOException {
-        String code = """
-                function mysum(x::Int64, y::Int64)::Int64
-                  total = x + y
-                  return total
-                end
+	@Test
+	public void defineFunctionAndCallTestMultipleDispatch() throws IOException {
+		final String code = "function mysum(x::Int64, y::Int64)::Int64\n" //
+				+ "  total = x + y\n" //
+				+ "  return total\n" //
+				+ "end\n" //
+				+ "\n" //
+				+ "function mysum(x::Float64, y::Float64)::Float64\n" //
+				+ "  total = x + y\n" //
+				+ "  return total\n" //
+				+ "end";
 
-                function mysum(x::Float64, y::Float64)::Float64
-                  total = x + y
-                  return total
-                end
-                """;
+		caller.ExecuteDefineFunction(code);
 
-        caller.ExecuteDefineFunction(code);
+		caller.Execute("jresult = mysum(3, 5)");
+		final int result = caller.getInt("jresult");
+		assertEquals(8, result);
 
-        caller.Execute("jresult = mysum(3, 5)");
-        int result = caller.getInt("jresult");
-        assertEquals(8, result);
+		caller.Execute("jresult = mysum(3.1, 5.1)");
+		final double resultdbl = caller.getDouble("jresult");
+		assertEquals(8.2, resultdbl);
 
-        caller.Execute("jresult = mysum(3.1, 5.1)");
-        double resultdbl = caller.getDouble("jresult");
-        assertEquals(8.2, resultdbl);
-
-    }
+	}
 }
--- src/test/java/org/expr/juliacaller/TestScriptEngine.java
+++ src/test/java/org/expr/juliacaller/TestScriptEngine.java
@@ -1,91 +1,95 @@
 package org.expr.juliacaller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import javax.script.*;
-import java.io.IOException;
-import static org.expr.juliacaller.TestBasics.caller;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 public class TestScriptEngine {
 
-    public static ScriptEngineManager manager;
-    public static ScriptEngine engine;
+	public static ScriptEngineManager manager;
+	public static ScriptEngine engine;
 
-    @BeforeAll
-    public static void init() {
-        // Constants.setProperties(Constants.JULIA_PATH, "/usr/local/bin/julia");
-        // Constants.setProperties(Constants.JULIA_PATH, "/usr/bin/julia");
-        Constants.setProperties(Constants.JULIA_PATH, Utilities.TryFindingJuliaExecutable());
-        Constants.setProperties(Constants.JULIA_PORT, "8002");
+	@BeforeAll
+	public static void init() {
+		// Constants.setProperties(Constants.JULIA_PATH, "/usr/local/bin/julia");
+		// Constants.setProperties(Constants.JULIA_PATH, "/usr/bin/julia");
+		Constants.setProperties(Constants.JULIA_PATH, Utilities.TryFindingJuliaExecutable());
+		Constants.setProperties(Constants.JULIA_PORT, "8002");
 
-        manager = new ScriptEngineManager();
-        engine = manager.getEngineByName("Julia");
-    }
+		manager = new ScriptEngineManager();
+		engine = manager.getEngineByName("Julia");
+	}
 
-    @Test
-    public void engineCreatedTest() {
-        assertTrue(engine != null);
-    }
+	@Test
+	public void engineCreatedTest() {
+		assertTrue(engine != null);
+	}
 
-    @Test
-    public void getAsJSONObject() throws IOException, ScriptException {
-        engine.eval("a = 3");
-        assertEquals(3, engine.get("a"));
-    }
+	@Test
+	public void getAsJSONObject() throws IOException, ScriptException {
+		engine.eval("a = 3");
+		assertEquals(3, engine.get("a"));
+	}
 
-    @Test
-    public void getAsJSONArray() throws ScriptException {
-        engine.eval("arr7 = [1,2,3]");
-        Object result = engine.get("arr7");
+	@Test
+	public void getAsJSONArray() throws ScriptException {
+		engine.eval("arr7 = [1,2,3]");
+		final Object result = engine.get("arr7");
 
-        assertTrue(result instanceof JSONArray);
+		assertTrue(result instanceof JSONArray);
 
-        JSONArray arr = (JSONArray) result;
-        assertEquals(1, arr.getInt(0));
-        assertEquals(2, arr.getInt(1));
-        assertEquals(3, arr.getInt(2));
-    }
+		final JSONArray arr = (JSONArray) result;
+		assertEquals(1, arr.getInt(0));
+		assertEquals(2, arr.getInt(1));
+		assertEquals(3, arr.getInt(2));
+	}
 
-    @Test
-    public void invokeFunctionTest() throws ScriptException, NoSuchMethodException {
-        Invocable invocable = (Invocable) engine;
-        engine.eval("using Statistics");
-        engine.eval("arr5 = [1.0, 2.0, 3.0]");
-        Object result = invocable.invokeFunction("mean", "arr5");
-        assertEquals(2.0, result);
-    }
+	@Test
+	public void invokeFunctionTest() throws ScriptException, NoSuchMethodException {
+		final Invocable invocable = (Invocable) engine;
+		engine.eval("using Statistics");
+		engine.eval("arr5 = [1.0, 2.0, 3.0]");
+		final Object result = invocable.invokeFunction("mean", "arr5");
+		assertEquals(new BigDecimal("2.0"), result);
+	}
 
-    @Test
-    public void invokeFunction2Test() throws ScriptException, NoSuchMethodException {
-        Invocable invocable = (Invocable) engine;
-        engine.eval("using Statistics");
-        engine.eval("arr2 = [1.0, 2.0, 3.0, 4.0, 5.0]");
-        engine.eval("f(x) = sum(x)/length(x)");
-        Object result = invocable.invokeFunction("f", "arr2");
-        assertEquals(3.0, result);
+	@Test
+	public void invokeFunction2Test() throws ScriptException, NoSuchMethodException {
+		final Invocable invocable = (Invocable) engine;
+		engine.eval("using Statistics");
+		engine.eval("arr2 = [1.0, 2.0, 3.0, 4.0, 5.0]");
+		engine.eval("f(x) = sum(x)/length(x)");
+		final Object result = invocable.invokeFunction("f", "arr2");
+		assertEquals(new BigDecimal("3.0"), result);
 
-    }
+	}
 
-    @Test
-    public void getDict() throws ScriptException {
-        engine.eval("d1 = Dict(\"a\" => [10,20,30], \"b\" => 3.14159265)");
-        Object result = engine.get("d1");
-        Object a = ((JSONObject) result).getJSONArray("a");
-        Object b = ((JSONObject) result).getDouble("b");
+	@Test
+	public void getDict() throws ScriptException {
+		engine.eval("d1 = Dict(\"a\" => [10,20,30], \"b\" => 3.14159265)");
+		final Object result = engine.get("d1");
+		final Object a = ((JSONObject) result).getJSONArray("a");
+		final Object b = ((JSONObject) result).getDouble("b");
 
-        assertEquals(3.14159265, b);
+		assertEquals(3.14159265, b);
 
-        JSONArray arr = (JSONArray) a;
-        assertEquals(10, arr.getInt(0));
-        assertEquals(20, arr.getInt(1));
-        assertEquals(30, arr.getInt(2));
+		final JSONArray arr = (JSONArray) a;
+		assertEquals(10, arr.getInt(0));
+		assertEquals(20, arr.getInt(1));
+		assertEquals(30, arr.getInt(2));
 
-    }
+	}
 
 }
